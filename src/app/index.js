0b19cbe12c86c5505383b9d01674a9a5c3f4c520
Ankit Pandey <ankit221601@gmail.com>
2025-02-23 23:38:14 +0530

    initialize index.js file

---
+++ src/app/index.js
@@ -0,0 +1,50 @@
+import {View, Text, StyleSheet, Image} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useFonts } from "expo-font";
+// importing images from assest
+import Logo from "../assets/images/icon.png"
+import { screenHeight, screenWidth } from "../utils/Constants";
+import { resetAndNavigate } from "../utils/Helpers";
+
+
+const Main = () => {
+
+    // linking fonts or loading fonts
+    const [loaded] = useFonts({
+        SpaceMono:require('../assets/fonts/SpaceMono-Regular.ttf')
+    })
+
+    // creating state
+    const [hasNavigated, setHasNavigated] = useState(false);
+
+    // creating useEffect
+    useEffect(() => {
+        if(loaded && !hasNavigated){
+            const timeoutId = setTimeout(() => {
+                resetAndNavigate("/home");
+            },1000);
+
+            return () => clearTimeout(timeoutId);
+        }
+    },[loaded,hasNavigated])
+
+    return (
+        <View style = {styles.container} >
+            <Image source={Logo} style = {styles.image} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    image: {
+        width: screenWidth * 0.3,
+        height: screenHeight * 0.12
+    }
+})
+
+export default Main;
