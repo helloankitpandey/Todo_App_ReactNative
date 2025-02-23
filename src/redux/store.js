7928016582797966f23fb06131a6d6f65fd400a6
Ankit Pandey <ankit221601@gmail.com>
2025-02-23 23:39:16 +0530

    Added redux folder

---
+++ src/redux/store.js
@@ -0,0 +1,29 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import rootReducer from "./rootReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+
+
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    blacklist:[],
+    whitelist:['todo']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [FLUSH, REGISTER, REHYDRATE, PAUSE, PERSIST, PURGE]
+            },
+        }),
+});
+
+export  const persistor = persistStore(store)
