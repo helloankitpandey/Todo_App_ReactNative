6a6b66df3ea4a885c4c148e1b9c9d1612a49b682
Ankit Pandey <ankit221601@gmail.com>
2025-06-02 23:52:07 +0530

    add styles

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -18,6 +18,8 @@ const styles = StyleSheet.create({
     container: {
         position:"absolute",
         bottom: 30,
+        right: 20,
+        backgroundColor: "#0090B0",
         padding: 15,
         borderRadius: 120,
         justifyContent: "center",
